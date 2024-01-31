786eccac60dcb58aa73dc861fa254b1e2bd9c94b
Kobe Ruado <insidiae423@gmail.com>
2024-01-31 17:59:29 +0800

    setup tailwind

--- express/src/index.ts
+++ express/src/index.ts
@@ -11,6 +11,7 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
---
+++ express/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./src/**/*.{ts,ejs}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
