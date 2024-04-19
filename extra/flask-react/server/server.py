c312a939b8381f36bbb318f47632e7bba3885c79
Kobe Ruado <insidiae423@gmail.com>
2024-04-19 16:03:51 +0800

    Add basic validation for Flask example

--- extra/flask-react/server/server.py
+++ extra/flask-react/server/server.py
@@ -3,11 +3,39 @@ import sqlite3
 from cuid2 import cuid_wrapper
 from flask import Flask, g, request, redirect, url_for
 from flask_cors import CORS
+from jsonschema import validate, Draft202012Validator, ValidationError
 from typing import Callable
-
 
 cuid_generator: Callable[[], str] = cuid_wrapper()
 
+TITLE_MAX_LENGTH = 100
+TAG_MAX_LENGTH = 25
+CONTENT_MAX_LENGTH = 10000
+
+post_editor_schema = {
+	"type": "object",
+	"properties": {
+		"title": {
+			"type": "string",
+			"minLength": 1,
+			"maxLength": TITLE_MAX_LENGTH,
+		},
+		"tags": {
+			"type": "array",
+			"items": {
+				"type": "string",
+				"minLength": 1,
+				"maxLength": TAG_MAX_LENGTH
+			}
+		},
+		"content": {
+			"type": "string",
+			"minLength": 1,
+			"maxLength": CONTENT_MAX_LENGTH
+		}
+	},
+	"required": ["title", "content"]
+}
 
 app = Flask(__name__)
 CORS(app)
@@ -33,7 +61,6 @@ def query_db(query, args=(), one=False):
 	cur.close()
 	return (rv[0] if rv else None) if one else rv
 
-
 def dict_factory(cursor, row):
 	fields = [column[0] for column in cursor.description]
 	return {key: value for key, value in zip(fields, row)}
@@ -44,13 +71,12 @@ def index():
 
 @app.route("/posts", methods=['GET', 'POST'])
 def posts():
-	print(request.method)
 	if request.method == "POST":
 		title = request.form["title"]
 		content = request.form["content"]
 		intent = request.form["intent"]
-		tags = []
 
+		tags = []
 		for key, value in request.form.items():
 			if key.startswith("tags[") and key.endswith("]"):
 				index = int(key[5:-1])
@@ -66,7 +92,7 @@ def posts():
 					"intent": intent,
 				}
 			}
-		
+
 		if intent.startswith("list-remove"):
 			index = int(intent.split("/")[1])
 			del tags[index]
@@ -78,26 +104,58 @@ def posts():
 					"intent": intent,
 				}
 			}
-		
+
 		if intent == "submit":
-			# TODO: Validate input data
-			new_post = (
-				cuid_generator(),
-				title,
-				",".join(tags),
-				content
-			)
-
-			conn = get_db()
-			conn.execute("""INSERT INTO Post(id, title, tags, content)
-			  VALUES(?,?,?,?) """, new_post)
-			conn.commit()
-
-			return {
-				"status": "success",
+			submission = {
+				"title": title,
+				"tags": tags,
+				"content": content
 			}
+
+			try:
+				validate(
+					instance=submission,
+					schema=post_editor_schema
+				)
+				new_post = (
+					cuid_generator(),
+					title,
+					",".join(tags),
+					content
+				)
+
+				conn = get_db()
+				conn.execute(
+					"""INSERT INTO Post(id, title, tags, content) VALUES(?,?,?,?) """, new_post
+				)
+				conn.commit()
+
+				return {
+					"status": "success",
+				}
+			except ValidationError:
+				validator = Draft202012Validator(post_editor_schema)
+				error_messages = {
+					"formErrors": [],
+					"fieldErrors": {
+						"title": [],
+						"tags": [],
+						"content": [],
+					},
+				}
+				errors = sorted(validator.iter_errors(submission), key=lambda e: e.path)
+				for error in errors:
+					path = error.path.popleft()
+					error_messages["fieldErrors"][path].append(error.message)
+					print(path, error.message)
+
+				return {
+					"status": "error",
+					"errors": error_messages,
+					"submission": submission,
+				}
 	else:
-		posts = query_db("select * from Post")
+		posts = query_db("SELECT * from Post")
 		return {
 			"posts": posts
 		}
